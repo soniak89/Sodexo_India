f6b2c1037c168fb2518c8efe27902665035b98b2
soniak89 <sona.sonia.k@gmail.com>
2022-07-07 15:23:19 +0530

    final commit

---
+++ src/test/java/Sodexo/SodexoIndia/mergerconflict.java
@@ -0,0 +1,34 @@
+package Sodexo.SodexoIndia;
+
+import org.testng.annotations.*;
+
+public class mergerconflict {
+	@Test
+	public void a()
+	{
+		System.out.println("A");
+		
+	}
+	@Test
+	public void B()
+	{
+		System.out.println("B");
+		
+	}
+
+	@Test
+	public void C()
+	{
+		System.out.println("C");
+		
+	}
+
+	@Test
+	public void D()
+	{
+		System.out.println("D");
+		
+	}
+
+
+}
